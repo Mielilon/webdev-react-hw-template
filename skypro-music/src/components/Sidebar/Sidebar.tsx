9e13cb7b6d78ddd611a2adae64e3ea8dfe4e7a49
Svetlana Kosuhina <142496854+Lanakosuhina@users.noreply.github.com>
2024-03-25 15:18:35 +0300

    components

--- skypro-music/src/components/Sidebar/Sidebar.tsx
+++ skypro-music/src/components/Sidebar/Sidebar.tsx
@@ -1,25 +1,15 @@
 'use client'
 
-import { Playlist } from "../Playlist"
 import playlist01 from "../../../public/img/playlist01.png"
 import playlist02 from "../../../public/img/playlist02.png"
 import playlist03 from "../../../public/img/playlist03.png"
 import styles from "../Sidebar/Sidebar.module.css"
-import { useEffect, useState } from "react"
-import SidebarSkeleton from "../SidebarSkeleton/SidebarSkeleton"
+import React, { Suspense } from "react"
 import SVG from "../SVG/SVG"
+import SidebarSkeleton from "../SidebarSkeleton/SidebarSkeleton"
 
 export default function Sidebar() {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000)
-    return () => {
-      clearTimeout(timeout);
-    }
-  }, [])
+  const Playlist = React.lazy(() => import('../Playlist/Playlist'))
 
   return (
     <div className={styles.mainSidebar}>
@@ -30,14 +20,13 @@ export default function Sidebar() {
         </div>
       </div>
       <div className={styles.sidebarBlock}>
-        {isLoading ? (
-          <SidebarSkeleton /> ) : (
-        <div className={styles.sidebarList}>
-          <Playlist src={playlist01} alt="day's playlist" />
-          <Playlist src={playlist02} alt="100 dance hits" />
-          <Playlist src={playlist03} alt="indi music playlist" />
-        </div>
-          )}
+        <Suspense fallback={<SidebarSkeleton />}>
+          <div className={styles.sidebarList}>
+            <Playlist src={playlist01} alt="day's playlist" />
+            <Playlist src={playlist02} alt="100 dance hits" />
+            <Playlist src={playlist03} alt="indi music playlist" />
+          </div>
+        </Suspense>
       </div>
     </div>
   )
